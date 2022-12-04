93b64aa9fdb1d3b1fc92bbf0c74e16f43584d030
Marcus Desai <marcus.desai@gmail.com>
2022-12-04 00:33:17 +0000

    write docs & improve readme

--- README.md
+++ README.md
@@ -5,18 +5,19 @@
 
 This small crate exports a single macro `variant!`, which can be used to destructure an expression into, and return assignments from, a single pattern. It works similarly to the [`matches!`][matches] macro from the rust std lib.
 
-This macro is mainly useful for reducing matching boilerplate when you just want to extract some values from a pattern match (my reason for making this 😉).
+This macro is mainly useful for reducing matching boilerplate when you just want to try and extract some values from a pattern match (my reason for making this 😉).
 
 ## Examples
 
 ```rust
 use variant::variant;
 
-let val = Ok(Some(0u8));
-let res: Result<u8, _> = variant!(val, Ok(Some(i)));
+let val = Some((0, 1));
+let res = variant!(val, Some((i, _))).expect("i");
+assert_eq!(res, 0);
 
-let err = variant!(val, Ok(1.0f32));
-assert!(err.is_err());
+let res = variant!(val, Some((10, j)));
+assert!(res.is_err());
 ```
 
 There are more examples on the [docs][docs] page
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,70 @@
+//! The [`variant`](`variant`) macro matches an expression against a given
+//! pattern returning a [`Result`](`core::result::Result`). If the pattern
+//! matches, then the `Ok` branch is returned including any assignments from
+//! the pattern (or [`unit`] if none are given). If the match fails then `Err`
+//! is returned with either a given error, or defaulting to
+//! `Box<dyn std::error::Error>` otherwise.
+//!
+//! ## Simple Example
+//!
+//! ```
+//! use variant::variant;
+//!
+//! let val = Some((0, 1));
+//! let res = variant!(val, Some((i, _))).expect("i");
+//! assert_eq!(res, 0);
+//!
+//! let res = variant!(val, Some((10, j)));
+//! assert!(res.is_err());
+//! ```
+//!
+//! ## Guards
+//!
+//! Conditional guards work the same as with [`matches!`][matches].
+//!
+//! ```
+//! use variant::variant;
+//!
+//! struct Foo {
+//!     a: usize,
+//!     b: Option<bool>,
+//! }
+//!
+//! let val = Foo { a: 20, b: None };
+//! let res = variant!(val, Foo { a, .. } if a == 20).expect("a");
+//! assert_eq!(res, 20);
+//!
+//! let res = variant!(val, Foo { b, .. } if b.is_some());
+//! assert!(res.is_err());
+//! ```
+//!
+//! ## Custom Errors
+//!
+//! ```
+//! use variant::variant;
+//!
+//! #[derive(Debug)]
+//! enum MyError {
+//!     Bad,
+//!     Worse,
+//!     Expensive,
+//! }
+//!
+//! let val = Some(1);
+//! let res = variant!(val, Some(i), MyError::Bad).expect("i");
+//! assert_eq!(res, 1);
+//!
+//! let res = variant!(val, Some(50), MyError::Worse);
+//! assert!(matches!(res, Err(MyError::Worse)));
+//!
+//! // We can also send an error returning closure with the following syntax
+//! let res = variant!(val, Some(50), else || MyError::Expensive);
+//! assert!(matches!(res, Err(MyError::Expensive)));
+//! ```
+//!
+//! [unit]: https://doc.rust-lang.org/std/primitive.unit.html
+//! [matches]: https://doc.rust-lang.org/std/macro.matches.html
+
 #[macro_export]
 macro_rules! variant {
     ($expression:expr, $pattern:pat_param $(if $guard:expr)?) => {
